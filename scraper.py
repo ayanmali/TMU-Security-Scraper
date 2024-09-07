3cbdda411979a8bb851bc53537aaf2f54dcfcc2d
Ayan Ali <ayan_ali2004@hotmail.com>
2024-09-07 18:56:01 -0400

    Added function for scraping incident details

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import requests
 import time
 from datetime import datetime
+import pandas as pd
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
 START_YEAR = 2020
 CURRENT_YEAR = datetime.now().year
 
+# List to store all JSON response data
 all_data = []
 
 """
@@ -97,22 +99,42 @@ def scrape_archived_incidents():
             # Getting the response and storing it
             response = requests.request("GET", url, data=payload, headers=headers)
             if response.status_code == 200:
-                all_data.extend(response.json())
+                # Parsing the JSON response and making sure there is at least one incident for the given month and year
+                if response.json().get('totalMatches', 0) > 0:
+                    all_data.extend(response.json())
+                else:
+                    print(f"No incidents for {month} {year}")
             else:
                 print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
             
             # Pausing to space out requests and avoid hitting limits
             time.sleep(10)
 
+def get_incident_details(df):
+    # Getting the 'page' columns
+    pages = df['page']
+    # Navigating to each individual incident's details page
+    for page in pages:
+        # Getting the part of each row in 'page' that contains the directory that points to the incident's details page
+        directory = page.split("list-of-security-incidents")[1]
+        url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents{directory}/"
+
+
+
 def main():
+    # Getting and storing incident data
     scrape_recent_incidents()
     scrape_archived_incidents()
+
+    # Creating a DataFrame to store the data
     df = pd.DataFrame(all_data)
     print(df)
     print(f"Total rows: {len(df)}")
 
     # Exporting the DataFrame as a .csv file
-    df.to_csv(f"TMU-Security-Incidents-{START_YEAR}-{CURRENT_YEAR}")
+    df.to_csv(f"TMU-Security-Incidents-{START_YEAR}-{CURRENT_YEAR}.csv")
+
+    get_incident_details(df)
 
 if __name__ == "__main__":
     main()
