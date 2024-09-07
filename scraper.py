4d332a04698a1e6052f7b131d842156750ba648b
Ayan Ali <ayan_ali2004@hotmail.com>
2024-09-07 17:43:36 -0400

    Stores response data in a DataFrame

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
 import requests
 import time
+from datetime import datetime
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+START_YEAR = 2020
+CURRENT_YEAR = datetime.now().year
+
+all_data = []
 
 """
 Scrapes security incidents that have occured in the current year.
@@ -34,7 +39,14 @@ def scrape_recent_incidents():
             page = 1
             url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.{page}.json"
 
-            requests.request("GET", url, data=payload, headers=headers)
+            # Getting the response and storing it
+            response = requests.request("GET", url, data=payload, headers=headers)
+            if response.status_code == 200:
+                all_data.extend(response.json())
+            else:
+                print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
+            
+            # Pausing to space out requests and avoid hitting limits
             time.sleep(10)
 
         # If the attempted page number doesn't exist, get an error
@@ -68,7 +80,7 @@ def scrape_archived_incidents():
     }
 
     # Getting data for each month from 2020-2023
-    for year in range(2020, 2024):
+    for year in range(START_YEAR, CURRENT_YEAR):
         for month in range(1, 12):
             # Formatting the month string
             if month < 10:
@@ -82,8 +94,25 @@ def scrape_archived_incidents():
             # Adjusting the request URL based on the year and month
             url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/{year}/{month}/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.1.json"
             
-            # Storing the response
-            requests.request("GET", url, data=payload, headers=headers)
+            # Getting the response and storing it
+            response = requests.request("GET", url, data=payload, headers=headers)
+            if response.status_code == 200:
+                all_data.extend(response.json())
+            else:
+                print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
+            
+            # Pausing to space out requests and avoid hitting limits
             time.sleep(10)
 
-print(scrape_current_incidents())
+def main():
+    scrape_recent_incidents()
+    scrape_archived_incidents()
+    df = pd.DataFrame(all_data)
+    print(df)
+    print(f"Total rows: {len(df)}")
+
+    # Exporting the DataFrame as a .csv file
+    df.to_csv(f"TMU-Security-Incidents-{START_YEAR}-{CURRENT_YEAR}")
+
+if __name__ == "__main__":
+    main()
