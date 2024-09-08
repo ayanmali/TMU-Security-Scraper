9c886d2e4f6e8b03ebc5c3d12bfc01a190fae533
Ayan Ali <ayan_ali2004@hotmail.com>
2024-09-07 22:41:04 -0400

    Added functions to scrape incident and description details

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import time
 from datetime import datetime
 import pandas as pd
@@ -35,25 +36,30 @@ def scrape_recent_incidents():
         "x-requested-with": "XMLHttpRequest"
     }
 
-    # Getting data from each page of results
-    for p in range(1, 7):
-        try:
-            page = 1
-            url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.{page}.json"
+    # Checking each page of results for data until there's no more left
+    p = 1
+    while True:
+        #page = 1
+        url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.{p}.json"
 
-            # Getting the response and storing it
-            response = requests.request("GET", url, data=payload, headers=headers)
-            if response.status_code == 200:
+        # Getting the response and storing it
+        response = requests.request("GET", url, data=payload, headers=headers)
+        if response.status_code == 200:
+            if response.json().get('data') != []:
+                print(f"Storing data from page {p}...")
                 all_data.extend(response.json())
             else:
-                print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
-            
-            # Pausing to space out requests and avoid hitting limits
-            time.sleep(10)
-
-        # If the attempted page number doesn't exist, get an error
-        except Exception:
-            print(f"Index {p} is out of bounds")
+                print(f"Endpoint for page {p} contains no data")
+                break
+        else:
+            print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
+            break
+        
+        # Incrementing the counter to get the next page of results
+        p += 1
+        
+        # Pausing to space out requests and avoid hitting limits
+        time.sleep(10)
 
 """
 Scrapes archived security incidents from previous years.
@@ -80,7 +86,7 @@ def scrape_archived_incidents():
         "user-agent": USER_AGENT,
         "x-requested-with": "XMLHttpRequest"
     }
-
+    
     # Getting data for each month from 2020-2023
     for year in range(START_YEAR, CURRENT_YEAR):
         for month in range(1, 12):
@@ -93,33 +99,98 @@ def scrape_archived_incidents():
             # Adjusting the referer header URL for the year and month
             headers['referer'] = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/{year}/{month}/"
 
-            # Adjusting the request URL based on the year and month
-            url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/{year}/{month}/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.1.json"
-            
-            # Getting the response and storing it
-            response = requests.request("GET", url, data=payload, headers=headers)
-            if response.status_code == 200:
-                # Parsing the JSON response and making sure there is at least one incident for the given month and year
-                if response.json().get('totalMatches', 0) > 0:
-                    all_data.extend(response.json())
+            p = 1
+            while True:
+                # Adjusting the request URL based on the year and month
+                url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents/{year}/{month}/jcr:content/content/restwocoltwoone/c1/ressecuritystack.data.1.json"
+                # Getting the response and storing it
+                response = requests.request("GET", url, data=payload, headers=headers)
+                if response.status_code == 200:
+                    # Parsing the JSON response and making sure there is at least one incident for the given month and year
+                    if response.json().get('totalMatches', 0) > 0 and response.json().get('data') != []:
+                        all_data.extend(response.json())
+                    else:
+                        print(f"No incidents for {month} {year} for page {p}")
+                        break
                 else:
-                    print(f"No incidents for {month} {year}")
-            else:
-                print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
-            
-            # Pausing to space out requests and avoid hitting limits
-            time.sleep(10)
+                    print(f"Failed to retrieve data from {url}: Status code {response.status_code}")
+                    break
+                
+                # Incrementing the counter to check the next page
+                p += 1
+                # Pausing to space out requests and avoid hitting limits
+                time.sleep(10)
 
-def get_incident_details(df):
+def get_details(df):
     # Getting the 'page' columns
     pages = df['page']
     # Navigating to each individual incident's details page
     for page in pages:
         # Getting the part of each row in 'page' that contains the directory that points to the incident's details page
         directory = page.split("list-of-security-incidents")[1]
+        # Getting the URL of the incident details page
         url = f"https://www.torontomu.ca/community-safety-security/security-incidents/list-of-security-incidents{directory}/"
 
+        # Getting the HTML content of the page
+        html = get_html_content(url)
+        if html:
+            print("HTML content retrieved successfully.")
+        else:
+            print("Failed to retrieve HTML content.")
 
+        # Creating the soup object to extract data from the HTML
+        soup = BeautifulSoup(html, 'html.parser')
+
+        incident_details = get_incident_details(soup)
+
+        description_details = get_description_details(soup)
+
+def get_incident_details(soup):
+    # Find the div with class "incidentdetails"
+    incident_div = soup.find('div', class_='incidentdetails')
+
+    if incident_div:
+        # Find the paragraph within this div
+        paragraph = incident_div.find('p')
+        
+        if paragraph:
+            return paragraph.text.strip()
+        else:
+            return "Paragraph not found within the incident details div."
+    else:
+        return "Incident details div not found."
+
+def get_description_details(soup):
+    # Find the div with class "description"
+    description_div = soup.find('div', class_='description')
+
+    if description_div:
+        # Find the paragraphs within this div
+        paragraphs = description_div.find_all('p')
+
+        if paragraphs:
+            # Get a list containing each paragraph of the description
+            return [p.text.strip() for p in paragraphs]
+        else:
+            return["No paragraphs found within the description div."]
+
+    else:
+        return ["Description div not found."]
+
+def get_html_content(url):
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+        
+        # Raise an exception for bad status codes
+        response.raise_for_status()
+        
+        # Return the HTML content
+        return response.text
+    
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
 
 def main():
     # Getting and storing incident data
@@ -134,7 +205,7 @@ def main():
     # Exporting the DataFrame as a .csv file
     df.to_csv(f"TMU-Security-Incidents-{START_YEAR}-{CURRENT_YEAR}.csv")
 
-    get_incident_details(df)
+    get_details(df)
 
 if __name__ == "__main__":
     main()
