a9ba7c7e009ac00758b2d95383d9b3238af13888
Ayan Ali <ayan_ali2004@hotmail.com>
2024-12-08 15:36:59 -0500

    Added scaler file, saved pytorch models, and postgres data to the repo

---
+++ django_api/tmu_security_site/postgres_params.py
@@ -0,0 +1,11 @@
+# HOST = "localhost"
+HOST = "db"
+# DBNAME = "TMU Security Data"
+DBNAME = "tmusecurity"
+#USER = "postgres"
+USER = "postgres"
+# PASSWORD = "ghau8msj860"
+PASSWORD = "postgres"
+PORT = 5432
+TABLE_NAME = "incidents"
+# DB_PARAMS = {'host':HOST, 'dbname':DBNAME, 'user':USER, 'password':PASSWORD}
